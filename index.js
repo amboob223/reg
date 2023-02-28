f65b586c2b124eb5dbeb80e73adae64e10b5b1c3
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-02-28 06:26:30 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ app.post("/gericka", async (req, res) => {
     }
 })
 
-app.listen(5000, () => {
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log("server works")
 })
